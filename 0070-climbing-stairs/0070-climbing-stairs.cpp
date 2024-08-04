3a24635339cfef06e928cb61c707cfe250a88b4e
Prahlad yadav <128887097+RusHerrr07@users.noreply.github.com>
2024-08-05 01:20:43 +0530

    Time: 3 ms (29.45%), Space: 7.7 MB (6.05%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+    vector<int> dp;
+    
+    int countWays(int n) {
+        if (n == 0) return 1;
+        if (n < 0) return 0;
+        if (dp[n] != -1) return dp[n];
+        dp[n] = countWays(n - 1) + countWays(n - 2);
+        return dp[n];
+    }
+
+public:
+    int climbStairs(int n) {
+        if (n == 0) return 1;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        dp.assign(n + 1, -1);
+        return countWays(n);
+    }
+};
