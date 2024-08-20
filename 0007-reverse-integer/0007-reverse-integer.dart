c6ae386d805b861a17365d41cb0d3a1bfbeb9ffb
Prahlad yadav <128887097+RusHerrr07@users.noreply.github.com>
2024-08-20 23:28:36 +0530

    Time: 426 ms (5.33%), Space: 153.2 MB (5.33%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.dart
@@ -0,0 +1,9 @@
+import 'dart:ffi';
+
+class Solution {
+  int reverse(int x) {
+    int a = x<0 ? -(int.parse((-x).toString().split('').reversed.join())):
+        int.parse(x.toString().split('').reversed.join());
+    return a >= 2147483648 || a < -2147483648 ? 0:a;
+  }
+}
