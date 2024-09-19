7b308b256ce4e618d9378d06ee5e226f03c0492a
Prahlad yadav <128887097+RusHerrr07@users.noreply.github.com>
2024-09-20 01:32:14 +0530

    Time: 2 ms (60.54%), Space: 9.1 MB (86.76%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,27 +1,27 @@
 
 class Solution {
-    long long factorial(int n) {
-        if (n < 0) return 0;
+    // long long factorial(int n) {
+    //     if (n < 0) return 0;
 
-        std::vector<long long> dp(n + 1, 1);
+    //     std::vector<long long> dp(n + 1, 1);
 
-        for (int i = 2; i <= n; ++i) {
-            dp[i] = dp[i - 1] * i;
-        }
+    //     for (int i = 2; i <= n; ++i) {
+    //         dp[i] = dp[i - 1] * i;
+    //     }
 
-        return dp[n];
-    }
+    //     return dp[n];
+    // }
 
 public:
     std::vector<std::vector<int>> permute(std::vector<int>& nums) {
         std::vector<std::vector<int>> ans;
         int n = nums.size();
-        long long fact = factorial(n);
+        // long long fact = factorial(n);
 
-        std::sort(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end());
         do {
             ans.push_back(nums);
-        } while (std::next_permutation(nums.begin(), nums.end()));
+        } while (next_permutation(nums.begin(), nums.end()));
 
         return ans;
     }
