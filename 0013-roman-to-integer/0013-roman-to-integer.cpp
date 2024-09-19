de6e1c223c8970c08e8db9a89912246a7ee36c77
Prahlad yadav <128887097+RusHerrr07@users.noreply.github.com>
2024-09-20 00:29:24 +0530

    Time: 10 ms (59.89%), Space: 14.2 MB (35.46%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        map<char, int> mp;
+        mp['I'] = 1;
+        mp['V'] = 5;
+        mp['X'] = 10;
+        mp['L'] = 50;
+        mp['C'] = 100;
+        mp['D'] = 500;
+        mp['M'] = 1000;
+
+        int n=s.size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(i+1<n && mp[s[i]]<mp[s[i+1]]){
+                ans-=mp[s[i]];
+            }
+            else{
+                ans+=mp[s[i]];
+            }
+        }
+
+        return ans;
+        
+    }
+};
