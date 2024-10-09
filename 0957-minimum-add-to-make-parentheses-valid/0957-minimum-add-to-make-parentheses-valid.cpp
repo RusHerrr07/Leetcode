e86ac8a15044d7eefa46d9d75948909cfea25a81
Prahlad yadav <128887097+RusHerrr07@users.noreply.github.com>
2024-10-09 15:26:06 +0530

    Time: 3 ms (42.94%), Space: 7.7 MB (81.46%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
+    
+        // int n = s.size();
+        // stack<char> st;
+        // for (auto &i : s) {
+        //     if (!st.empty() && i == ')' && st.top() == '(') {
+        //         st.pop();
+        //     } else {
+        //         st.push(i);
+        //     }
+        // }
+        // return st.size();
+
+
+
+        
+
+
+        // optimal----->with o(1) space and o(n) time;
         int temp=0;
         int cnt=0;
         int n=s.size();
