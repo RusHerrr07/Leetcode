b0f061815d4ff4cb202f592a9cda0fea2971c367
Prahlad yadav <128887097+RusHerrr07@users.noreply.github.com>
2024-12-28 01:14:35 +0530

    Time: 31 ms (14.18%), Space: 31.1 MB (5.35%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
@@ -1,20 +1,19 @@
-
 class Solution {
 public:
     string shortestCommonSupersequence(string str1, string str2) {
         int n=str1.length(),m=str2.length();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        vector<vector<int>> sonal(n+1,vector<int>(m+1,0));
         for(int i=0;i<=n;i++)
             for(int j=0;j<=m;j++)
-                if(i==0) dp[i][j]=j;
-                else if(j==0) dp[i][j]=i;
-                else if(str1[i-1]==str2[j-1]) dp[i][j]=1+dp[i-1][j-1];
-                else dp[i][j]=1+min(dp[i-1][j],dp[i][j-1]);
+                if(i==0) sonal[i][j]=j;
+                else if(j==0) sonal[i][j]=i;
+                else if(str1[i-1]==str2[j-1]) sonal[i][j]=1+sonal[i-1][j-1];
+                else sonal[i][j]=1+min(sonal[i-1][j],sonal[i][j-1]);
         string scs="";
         int i=n,j=m;
         while(i>0&&j>0) {
             if(str1[i-1]==str2[j-1]) {scs=str1[i-1]+scs;i--;j--;}
-            else if(dp[i-1][j]<dp[i][j-1]) {scs=str1[i-1]+scs;i--;}
+            else if(sonal[i-1][j]<sonal[i][j-1]) {scs=str1[i-1]+scs;i--;}
             else {scs=str2[j-1]+scs;j--;}
         }
         while(i>0) {scs=str1[i-1]+scs;i--;}
