74746fc214882ad9d6572c38de1fa55af8b18fbe
Prahlad yadav <128887097+RusHerrr07@users.noreply.github.com>
2025-01-05 20:41:31 +0530

    Time: 0 ms (100%), Space: 40.9 MB (74.25%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -1,14 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        // Base cases
-        if (p == null && q == null) return true; // Both trees are null
-        if (p == null || q == null) return false; // One tree is null
-        
-        // Compare current nodes and recursively compare subtrees
-        boolean cond1 = (p.val == q.val); // Compare values using '=='
-        boolean cond2 = isSameTree(p.left, q.left); // Compare left subtree
-        boolean cond3 = isSameTree(p.right, q.right); 
+       
+        if (p==null&&q==null)return true;
+        if (p==null||q==null)return false;
+        boolean cond1=Integer.valueOf(p.val).equals(Integer.valueOf(q.val));
+        boolean cond2=isSameTree(p.left,q.left);
+        boolean cond3=isSameTree(p.right,q.right);
 
-        return cond1 && cond2 && cond3; 
+        return cond1&&cond2&&cond3;
     }
 }
