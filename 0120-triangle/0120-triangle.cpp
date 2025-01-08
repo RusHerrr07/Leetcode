025677bc084de45c41cead3789c064237d0fc395
Prahlad yadav <128887097+RusHerrr07@users.noreply.github.com>
2025-01-09 00:03:09 +0530

    Time: 0 ms (100%), Space: 12.5 MB (68.04%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,67 +1,23 @@
 class Solution {
-
-int dp[201][201];
-    
-int solve(vector<vector<int>>& triangle, int i, int j, int n, int m) {
-    if (i == n - 1) {
-        return triangle[i][j];
-    }
-    if (i >= n ) {
-        return INT_MAX;
-    }
-    if (dp[i][j] != -1) {
-        return dp[i][j];
-    }
-    int first = INT_MAX, sec = INT_MAX;
-    
-        first = solve(triangle, i + 1, j + 1, n, m);
-    
-    sec = solve(triangle, i + 1, j, n, m);
-    dp[i][j] = min(first, sec)+triangle[i][j];
-    return dp[i][j];
-}
 public:
-    int minimumTotal(vector<vector<int>>& triangle) {
-        /*
-
-        how greedy failed in this problem, just look at there--->
-
-
-
-
-
-        int ans = triangle[0][0], n = triangle.size(), i = 0, j = 0;
-        while (i + 1 < n) {
-            int first = INT_MAX, sec = INT_MAX;
-            if (i + 1 < n) {
-                sec = triangle[i + 1][j];
-                if (j + 1 < triangle[i + 1].size()) {
-                    first = triangle[i + 1][j + 1];
-                }
-            }
-            if (first < sec) {
-                ans += first;
-                i++;
-                j++;
-            } else {
-                ans += sec;
-                i++;
-            }
+    int dp[201][201];
+    int solve(vector<vector<int>>& triangle,int i,int j,int n){
+        if(i==n){
+            return 0;
         }
-        return ans;
-
-        */
-
-
-        // now let's try dp with memoization-->
-
-        int n = triangle.size();
-        int m = triangle[0].size();
-        memset(dp, -1, sizeof(dp));
-        return solve(triangle, 0, 0, n, m);
-
-    
-
-
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+      
+        int first=solve(triangle,i+1,j,n)+triangle[i][j];;
+        int sec=solve(triangle,i+1,j+1,n)+triangle[i][j];
+        dp[i][j]=min(first,sec);
+        return dp[i][j];
+        
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        memset(dp,-1,sizeof(dp));
+        int n=triangle.size();
+        return solve(triangle,0,0,n);
     }
 };
