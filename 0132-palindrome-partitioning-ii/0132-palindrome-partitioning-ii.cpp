292716ffc9d6c1f3ab63c8bd3d01c094c7febd5c
Prahlad yadav <128887097+RusHerrr07@users.noreply.github.com>
2025-01-10 01:32:24 +0530

    Time: 1372 ms (48.21%), Space: 9.1 MB (87.57%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,26 +1,24 @@
 class Solution {
-    
-    bool isPalindrome(string &s, int i, int j) {
-        while (j >= i) {
-            if (s[i] != s[j]) return false;
-            i++; 
-            j--;
-        }
-        return true;
-    }
-
 public:
     int minCut(string &s) {
         int n = s.size();
-        vector<int> dp(n + 1, INT_MAX);
-        dp[0] = -1;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                int take = 100000000000;
-                if (isPalindrome(s, j, i)) {
-                    take = 1 + dp[j];
+        vector<int> dp(n+1,INT_MAX);
+        dp[0]=-1;
+        auto PalindromeHaiKya =[](const string &s,int i,int j)->bool {
+           while(j>=i) {
+           if (s[i]!=s[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+       };
+       for (int i=0; i<n;i++) {
+            for (int j=0; j<=i; j++) {
+                int take =INT_MAX;
+                if (PalindromeHaiKya(s,j,i)) {
+                    take =1+dp[j];
                 }
-                dp[i + 1] = min(dp[i + 1], take);
+                dp[i+1]=min(dp[i+1],take);
             }
         }
         return dp[n];
