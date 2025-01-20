cc9c1bc17ad36898b5a398d4a56bc129cc372727
Prahlad yadav <128887097+RusHerrr07@users.noreply.github.com>
2025-01-21 02:10:21 +0530

    Time: 77 ms (67.73%), Space: 112.9 MB (75.25%) - LeetHub

---
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
@@ -0,0 +1,37 @@
+class Solution {
+int dx[4]={0,+1,0,-1};
+int dy[4]={-1,0,+1,0};
+public:
+    vector<vector<int>> highestPeak(vector<vector<int>>& mat) {
+
+        int n=mat.size();
+        int m=mat[0].size();
+        queue<pair<int,int>> q;
+        vector<vector<int>> ans(n,vector<int>(m,-1));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==1){
+                    q.push({i,j});
+                    ans[i][j]=0;
+                }
+            }
+        }
+
+        while(!q.empty()){
+             int i=q.front().first;
+             int j=q.front().second;
+             q.pop();
+             for(int dir=0;dir<4;dir++){
+                int x=i+dx[dir];
+                int y=j+dy[dir];
+                if(x>=0 && x<n && y>=0 && y<m && ans[x][y]==-1){
+                    ans[x][y]=ans[i][j]+1;
+                    q.push({x,y});
+                }
+             }
+        }
+
+        return ans;
+        
+    }
+};
