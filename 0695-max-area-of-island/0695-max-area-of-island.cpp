c3e17d23f7daa71d07ff7930f6dc346ac69f71c3
Prahlad yadav <128887097+RusHerrr07@users.noreply.github.com>
2025-01-28 12:24:08 +0530

    Time: 0 ms (100%), Space: 27.3 MB (91.32%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -3,7 +3,29 @@ private:
 int maxi =0;
 int dx[4] ={0,0,-1,1};
 int dy[4] ={-1,1,0,0};
- int DFS(vector<vector<int>>& grid, int i, int j, int n, int m) {
+ int BFS(vector<vector<int>>& grid,int i,int j,int n,int m) {
+        queue<pair<int, int>> q;
+        int ans = grid[i][j];
+        q.push({i, j});
+        grid[i][j] = 0;
+
+        while (!q.empty()) {
+            int row =q.front().first;
+            int col=q.front().second;
+            q.pop();
+            for (int dir=0; dir<4;dir++) {
+                int nrow = row + dx[dir], ncol=col+dy[dir];
+                if (nrow >= 0 && nrow <n&&ncol>=0 && ncol<m && grid[nrow][ncol] != 0) {
+                    ans += grid[nrow][ncol];
+                    q.push({nrow, ncol});
+                    grid[nrow][ncol] = 0;
+                }
+            }
+        }
+        return ans;
+    }
+
+    int DFS(vector<vector<int>>& grid, int i, int j, int n, int m) {
     if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == 0) 
         return 0;
 
@@ -17,7 +39,6 @@ int dy[4] ={-1,1,0,0};
 
     return ans;
 }
-
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
           int n = grid.size(), m = grid[0].size();
