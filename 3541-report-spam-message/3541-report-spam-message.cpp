b20c9e43cfd5433bc52ef4f2a162404830672b8e
Prahlad yadav <128887097+RusHerrr07@users.noreply.github.com>
2025-02-03 17:05:30 +0530

    Time: 570 ms (16.09%), Space: 290.8 MB (47.82%) - LeetHub

--- 3541-report-spam-message/3541-report-spam-message.cpp
+++ 3541-report-spam-message/3541-report-spam-message.cpp
@@ -1,3 +1,59 @@
+/* class TrieNode{
+public:
+    unordered_map<char,TrieNode*> c;
+    bool e;
+    int cnt;
+    TrieNode():e(false),cnt(0){}
+};
+
+class Trie{
+public:
+    TrieNode* r;
+    Trie(){
+        r=new TrieNode();
+    }
+    void insert(string& w){
+        TrieNode* n=r;
+        for(char ch:w){
+            if(n->c.find(ch)==n->c.end())
+                n->c[ch]=new TrieNode();
+            n=n->c[ch];
+        }
+        n->e=true;
+        n->cnt++;
+    }
+    int search(const string& w){
+        TrieNode* n=r;
+        for(char ch:w){
+            if(n->c.find(ch)==n->c.end()) return 0;
+            n=n->c[ch];
+        }
+        return n->cnt;
+    }
+};
+
+class Solution{
+public:
+    bool reportSpam(vector<string>& m,vector<string>& b){
+        unordered_set<string> s(b.begin(),b.end());
+        Trie t;
+        for(string& x:m)
+            t.insert(x);
+        int c=0;
+        for(const string& x:s){
+            int y=t.search(x);
+            if(y!=0) c+=y;
+            if(c>=2) return true;
+        }
+        return false;
+    }
+};
+
+*/
+
+
+// Easy and best Approach use hashSet of HashMap-->
+
 class Solution {
 public:
     bool reportSpam(vector<string>& message, vector<string>& banned) {
