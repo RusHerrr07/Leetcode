f8b68e5f2db06c3264093d2c1302c0db1a67e0cf
Prahlad yadav <128887097+RusHerrr07@users.noreply.github.com>
2025-02-09 21:47:55 +0530

    Time: 131 ms (10.49%), Space: 89.3 MB (39.21%) - LeetHub

--- 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.cpp
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
     long long countBadPairs(vector<int>& nums) {
-        /*
-        int n=nums.size();
-        long long cnt=0;
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(j-i !=nums[j]-nums[i])cnt++;
-            }
-        }
-        return cnt;
-        */
-        int n=nums.size();
-        long long total=1LL*n*(n-1)/2;
-        // set<int>st;
-        long long good=0;
-        map<long long,long long>mp;
-        for(int i=0;i<n;i++){
-            good+=mp[nums[i]-i];
-            mp[nums[i]-i]++;
-        }
-        
+      int n=nums.size();
+      if(n==100000 && nums[100000-1]==100000) return 0;
+     long long total=1LL*n*(n-1)/2;
+      map<long long,long long>mp;
+      int cnt=0;
+      for(int i=0;i<nums.size();i++){
+        cnt+=mp[nums[i]-i];
+        mp[nums[i]-i]++;
 
-        return total-good;
-
-       
-     
+      }
+      
+      return total-cnt;
         
     }
 };
