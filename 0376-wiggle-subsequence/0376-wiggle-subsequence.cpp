276681cf5e03c277158d0a55fbca2e2e7146aa7d
Prahlad yadav <128887097+RusHerrr07@users.noreply.github.com>
2025-03-04 00:13:31 +0530

    Time: 3 ms (26.48%), Space: 10.2 MB (37.96%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
@@ -1,36 +1,70 @@
 class Solution {
+int dp[1000+1][3];  
+int solve(vector<int>&nums,int idx,int n,bool flag){  
+    if(idx>=n )return 0;  
+    if(dp[idx][flag]!=-1)return dp[idx][flag];  
+    int take=0;  
+    if(flag && nums[idx]-nums[idx-1]<0){  
+        take=1+solve(nums,idx+1,n,!flag);    
+    }  
+    else if(!flag && nums[idx]-nums[idx-1]>0){  
+        take=1+solve(nums,idx+1,n,!flag);      
+    }  
+    int not_take=solve(nums,idx+1,n,flag);    
+    return dp[idx][flag]=max(take,not_take);  
+}  
 public:
     int wiggleMaxLength(vector<int>& nums) {
-        
-        int n=nums.size();
-        if(n==1)return 1;
-        int val=nums[0]-nums[1];
+    
+      //Greedy--->
 
-        if(n==5 && nums[0]==3 && nums[1]==3 && nums[n-1]==5)return 3;
+
+    //     int n=nums.size();
+    //     if(n==1)return 1;
+    //     int val=nums[0]-nums[1];
+
+    //     if(n==5 && nums[0]==3 && nums[1]==3 && nums[n-1]==5)return 3;
        
-       bool flag=false;
-       int cnt=1;
-        for(int i=1;i<n;i++){
-            int diff=nums[i]-nums[i-1];
-            if(i==1){
+    //    bool flag=false;
+    //    int cnt=1;
+    //     for(int i=1;i<n;i++){
+    //         int diff=nums[i]-nums[i-1];
+    //         if(i==1){
                
-               if(diff!=0){
-                 flag=diff>0;
-                 cnt++;
-               }
+    //            if(diff!=0){
+    //              flag=diff>0;
+    //              cnt++;
+    //            }
                 
-            }
-            else{
+    //         }
+    //         else{
                
-               bool temp=(diff>0);
-                if (diff!=0 &&temp!=flag) {
-                    cnt++;
-                    flag=temp;
-                }
+    //            bool temp=(diff>0);
+    //             if (diff!=0 &&temp!=flag) {
+    //                 cnt++;
+    //                 flag=temp;
+    //             }
 
-            }
-        }
+    //         }
+    //     }
 
-        return cnt;
+    //     return cnt;
+
+
+
+    //Dp-->
+    memset(dp,-1,sizeof(dp));
+    int n=nums.size();
+
+
+
+    int ans1=solve(nums,1,n,0)+1;
+    int ans2=solve(nums,1,n,1)+1;
+    return max(ans1,ans2);
+
+
+
+
+
      }
 };
