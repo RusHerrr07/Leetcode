9422fe40829d3182b5faa26bc702444c3406a9af
Prahlad yadav <128887097+RusHerrr07@users.noreply.github.com>
2025-03-14 22:19:01 +0530

    Time: 177 ms (25.28%), Space: 112 MB (14.12%) - LeetHub

--- 3351-maximize-happiness-of-selected-children/3351-maximize-happiness-of-selected-children.cpp
+++ 3351-maximize-happiness-of-selected-children/3351-maximize-happiness-of-selected-children.cpp
@@ -2,21 +2,36 @@ class Solution {
 public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
 
-        sort(happiness.begin(),happiness.end());
-        int n=happiness.size();
+        // sort(happiness.begin(),happiness.end());
+        // int n=happiness.size();
+        // int cnt=0;
+        // int idx=n-1;
+        // long long ans=0;
+        // while(idx>=0 && k>0){
+        //     int val=(happiness[idx]-cnt)>=0?(happiness[idx]-cnt):0;
+        //     ans+=val;
+        //     cnt++;
+        //     k--;
+        //     idx--;
+        // }
+
+        // return ans;
+
+        
+        priority_queue<int> pq(happiness.begin(), happiness.end());
         int cnt=0;
-        int idx=n-1;
         long long ans=0;
-        while(idx>=0 && k>0){
-            int val=(happiness[idx]-cnt)>=0?(happiness[idx]-cnt):0;
+        
+        while(!pq.empty() && k>0){
+            int val=(pq.top()-cnt)>=0?(pq.top()-cnt):0;
             ans+=val;
+            pq.pop();
             cnt++;
             k--;
-            idx--;
         }
 
         return ans;
-
     
+        
     }
 };
