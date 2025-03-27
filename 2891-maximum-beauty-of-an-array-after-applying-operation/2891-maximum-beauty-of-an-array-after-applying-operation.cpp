188ed928c896de526654afa061dc26c4fc028993
Prahlad yadav <128887097+Prahlad-07@users.noreply.github.com>
2025-03-27 17:34:27 +0530

    Time: 141 ms (22.2%), Space: 141.8 MB (18.01%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
     int maximumBeauty(vector<int>& nums, int k) {
-
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        int maxi=0;
-        int i=0;
-        int j=0;
-        while(j<n){
-            while(j<n && nums[j]-nums[i]<=2*k){
-                j++;
-                maxi=max(maxi,j-i);
-            }
-            i++;
+        int n = nums.size();
+        vector<pair<int, int>> p;
+        for (auto &i : nums) {
+            p.push_back({i - k, i + k});
+        }
+        vector<int> start, end;
+        for (auto &[fir, sec] : p) {
+            start.push_back(fir);
+            end.push_back(sec);
+        }
+        sort(start.begin(), start.end());
+        sort(end.begin(), end.end());
+        int maxi = 0;
+        for (int i = 0; i < start.size(); i++) {
+            int val = end[i];
+            int idx = upper_bound(start.begin(), start.end(), val) - start.begin();
+            // if(start[idx]==val){
+            //     maxi=max(maxi,idx-i);
+            // }
+            idx -= 1;
+            maxi = max(maxi, idx - i + 1);
         }
         return maxi;
-        
     }
 };
