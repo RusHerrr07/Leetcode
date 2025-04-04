c59f95e5a6b35d2623767b8eb7bed5d310032536
Prahlad yadav <128887097+Prahlad-07@users.noreply.github.com>
2025-04-05 02:41:52 +0530

    Time: 0 ms (100%), Space: 16.3 MB (32.86%) - LeetHub

---
+++ 0896-smallest-subtree-with-all-the-deepest-nodes/0896-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    pair<TreeNode*, int> solve(TreeNode* root, int depth){
+        if(root==NULL) return {root, depth+1};
+
+        pair<TreeNode*, int> l = solve(root->left, depth+1);
+        pair<TreeNode*, int> r = solve(root->right, depth+1);
+
+        if(l.second<r.second) return r;
+        else if(l.second>r.second) return l;
+        return {root, l.second};
+    }
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        return solve(root, 0).first;
+    }
+};
