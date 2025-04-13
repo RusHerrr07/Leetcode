52059701fe7f28193b080f5eab987633d5a1e12a
Prahlad yadav <128887097+Prahlad-07@users.noreply.github.com>
2025-04-13 11:54:04 +0530

    Time: 3 ms (49.45%), Space: 40.2 MB (23.79%) - LeetHub

--- 1428-jump-game-iii/1428-jump-game-iii.cpp
+++ 1428-jump-game-iii/1428-jump-game-iii.cpp
@@ -1,4 +1,5 @@
 class Solution {
+vector<int>dp;
 private:
     bool solve(vector<int>& arr, int idx, int n) {
         if (idx < 0 || idx >= n || arr[idx] < 0) return false;
@@ -6,14 +7,16 @@ private:
         
         int jump = arr[idx];
         arr[idx] = -arr[idx]; 
+        if(dp[idx]!=-1)return dp[idx];
         
         bool aage = solve(arr, idx + jump, n);
         bool piche = solve(arr, idx - jump, n);
-        return aage || piche;
+        return  dp[idx]=aage || piche;
     }
 
 public:
     bool canReach(vector<int>& arr, int start) {
+        dp.assign(arr.size(),-1);
         return solve(arr, start, arr.size());
     }
 };
