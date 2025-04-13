d33d11c52a3f22ecf85e88be53a9efb60f12273b
Prahlad yadav <128887097+Prahlad-07@users.noreply.github.com>
2025-04-13 13:35:58 +0530

    Time: 16 ms (22.92%), Space: 19.9 MB (83.16%) - LeetHub

--- 2001-jump-game-vii/2001-jump-game-vii.cpp
+++ 2001-jump-game-vii/2001-jump-game-vii.cpp
@@ -4,11 +4,15 @@ public:
         int n = s.size();
         vector<bool> dp(n, false);
         dp[0] = true;
-        int reachable = 0;
+        int ohk = 0;
         for (int i = 1; i < n; ++i) {
-            if (i >= minJump) reachable += dp[i - minJump];
-            if (i > maxJump) reachable -= dp[i - maxJump - 1];
-            dp[i] = (s[i] == '0' && reachable > 0);
+            if (i >= minJump) {
+                ohk += dp[i - minJump];
+                }
+            if (i > maxJump) {
+                ohk -= dp[i - maxJump - 1];
+                }
+            dp[i] = (s[i] == '0' && ohk > 0);
         }
         return dp[n - 1];
     }
