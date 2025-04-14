0ac25e76cd90b696d4244d6c4b4e6cf0fa558ebd
Prahlad yadav <128887097+Prahlad-07@users.noreply.github.com>
2025-04-14 13:28:26 +0530

    Time: 19 ms (20.53%), Space: 22.9 MB (8.79%) - LeetHub

--- 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
@@ -19,12 +19,40 @@ public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         int n = graph.size();
         adj = graph;  
-        DFS(0, n - 1);
+        // DFS(0, n - 1);
 
-        vector<vector<int>> ans;
-        for (auto &i : st) {
-            ans.push_back(i);
-        }
-        return ans;
+        // vector<vector<int>> ans;
+        // for (auto &i : st) {
+        //     ans.push_back(i);
+        // }
+        // return ans;
+
+
+
+        //let's solve using BFS;
+         vector<vector<int>> ans;
+         queue<vector<int>>q;
+         vector<int>path;
+         path.push_back(0);
+
+         q.push(path);
+         while(!q.empty()){
+            auto arr=q.front();
+            q.pop();
+            if(arr.back()==n-1){
+                ans.push_back(arr);
+            }
+            else{
+                int node=arr.back();
+                for(auto &neigh:adj[node]){
+                    vector<int>naya=arr;
+                    naya.push_back(neigh);
+                    q.push(naya);
+                }
+            }
+         }
+
+         return ans;
+
     }
 };
